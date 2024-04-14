cccd43ae7eab8896ef1446c3a8975c46cc824234
Soham1511 <125867744+Soham1511@users.noreply.github.com>
2024-04-14 05:19:00 +0000

    conversions of Data types

---
+++ basics/conversion.js
@@ -0,0 +1,24 @@
+// let score = 33;
+// console.log(typeof score);
+
+// let score = "Soham";
+//console.log(typeof score);
+
+// let valInNumber = Number(score);
+//console.log(valInNumber);
+
+/*
+"33" => 33
+true => 1 , false => 0 and viceVersa
+"" => false
+"Soham" => true
+*/
+
+// let isLoggedIn = "Soham";
+// let boolIsLoggedIn = Boolean(isLoggedIn);
+// console.log(boolIsLoggedIn);
+
+let num = 7;
+let numToString = String(num);
+console.log(numToString);
+console.log(typeof numToString);
