f51958763f1565612c9bf733f8cc4d1cade679f8
Soham1511 <125867744+Soham1511@users.noreply.github.com>
2024-04-14 04:51:53 +0000

    variables JS

---
+++ basics/variables.js
@@ -0,0 +1,6 @@
+let employeeId = 432456543;
+let employeeName = "Soham";
+let employeeCity = "Kolhapur";
+const password = "3456786sdfgh";
+
+console.table([employeeId, employeeName, employeeCity, password]);
