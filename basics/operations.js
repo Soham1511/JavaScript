1055a5852d2a5a5596429650ab6f0c1bf6e5b199
Soham1511 <125867744+Soham1511@users.noreply.github.com>
2024-04-14 05:42:30 +0000

    operations of data Types in js

---
+++ basics/operations.js
@@ -0,0 +1,15 @@
+let val = 7;
+let negVal = -val;
+// console.log(negVal);
+
+let str1 = "Hello";
+let str2 = " Soham";
+
+// console.log(str1 + str2);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "3");
+
+console.log(+true)
