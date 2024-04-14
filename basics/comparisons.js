864b6bf27a3824deb2fc2f909d3a9c7fc966603b
Soham1511 <125867744+Soham1511@users.noreply.github.com>
2024-04-14 06:06:36 +0000

    comparisons of data Types and operators in js

---
+++ basics/comparisons.js
@@ -0,0 +1,15 @@
+// console.log(2 == 2);
+// console.log(2 >= 2);
+// console.log(2 <= 2);
+// console.log(2 != 2);
+// console.log(2 > 2);
+// console.log(2 < 2);
+
+// console.log("02" > 1); //comaparisons of diff dataTypes
+
+console.log(null > 0); //false
+console.log(null === 0); //false
+console.log(null >= 0); //true because,comparisons operator converts null to a number thats why null >= 0 is true
+                        // "==" operator and "< > <= >=" works differently.
+
+
