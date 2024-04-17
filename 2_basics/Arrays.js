6b2adeef53cef57a5dfe50934cd5d95d1323a35a
Soham1511 <125867744+Soham1511@users.noreply.github.com>
2024-04-17 09:45:01 +0530

    Delete 2_basics directory

--- 2_basics/Arrays.js
+++
@@ -1,6 +0,0 @@
-//java script arrays are resizable
-const myArr = [1,2,3,4,5];
-console.log(myArr[3]);
-
-myArr.push(6);
-console.log(myArr);
--- 2_basics/arrays.js
+++
@@ -1,3 +0,0 @@
-//java script arrays are resizable
-const myArr = [1,2,3,4,5];
-console.log(myArr[3]);
