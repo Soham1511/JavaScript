f3549c95f902c1092b176f79ef1e8c2477bb4ef7
Soham1511 <125867744+Soham1511@users.noreply.github.com>
2024-06-03 14:13:20 +0000

    Array.js

--- basics/Array.js
+++ basics/Array.js
@@ -14,4 +14,16 @@ const spread = [...marvel]; // if u use single array and ... then nothing will c
 console.log(spread);
 
 let arr = [1,2,4,5,[7,9,3,8],9,34,46,29,[12,43,64],2];
-console.log 
+console.log(arr);
+console.log(arr.flat(Infinity)); //will display one array with all subarray elements just give depth infinity.
+
+
+console.log(Array.isArray("Soham")) //asked question about it is array or not will return true or false
+// console.log(Array.from("Soham")); // will convert string to array
+
+console.log(Array.from({name : "soham"})); // do not process {} will give empty []
+
+let score1 = 100;
+let score2 = 200;
+let score3 = 300;
+console.log(Array.of(score1, score2, score3)); // Returns a new array from a set of elements. like var, obj, array 
