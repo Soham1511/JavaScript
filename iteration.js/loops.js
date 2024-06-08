a1ba9103e6997d10dcd81092e7e6de9cec03774f
Soham1511 <125867744+Soham1511@users.noreply.github.com>
2024-06-08 17:14:44 +0000

    for loop, break and continue

---
+++ iteration.js/loops.js
@@ -0,0 +1,40 @@
+//for
+
+for(let i = 0; i<10; i++)
+    {
+        const val = i;
+        //console.log(val);
+    }
+
+
+for(let i = 0; i<10;i++)
+    {
+        //console.log(`outer loop val: ${i}`);
+        for(let j = 0;j<10;j++)
+            {
+                //console.log(`inner loop val: ${j} and inner loop is ${i}`)
+                // console.log(`${i} * ${j} = ${i*j}`);
+            }
+    }
+
+
+// loops on arrays ->
+
+let arr = ["iron man", "thor", "spider man", "hulk"];
+for(let i = 0; i<arr.length; i++)
+    {
+        //console.log(arr[i]);
+    }
+
+// break and continue
+
+for (let i = 1 ; i<=20 ; i++)
+    {
+        if(i == 15)
+            {
+                console.log("15 detected");
+                // break; it immediattly comes out of loop after conditon is achived
+                continue; // it continues to print the result after if statement
+            }
+        console.log(i)
+    }
