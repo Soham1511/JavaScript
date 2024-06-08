ce70abce8ee5338653ed8df4f5b86e24653e7fa7
Soham1511 <125867744+Soham1511@users.noreply.github.com>
2024-06-08 06:51:01 +0000

    truthy and falsy val / nullish coalescing / terinary operator

--- Control_Flow/switch.js
+++ Control_Flow/switch.js
@@ -18,3 +18,30 @@ switch (month) {
         console.log("default case");
         break;
 }  
+
+//************************************ Truthy and falsy value ************************************* */
+let userEmail = "sam@gmail.com";
+if(userEmail)
+    {
+        console.log("got user email");
+    } 
+else{
+    console.log("not got the user email")
+}
+
+// considerd as a falsy val -> false, 0, -0, BigInt 0n, NaN, undefined, null and "" are the falsy value 
+// considerd as a truthy val -> "0", "false", " ", [], {}, function(){}
+
+// Nullish coalescing operator(??) specialy used for handling null and undefined 
+
+let a;
+// a = 5 ?? 10 // will assign 5 to variable a
+a = null ?? 10 // this is the case for which this nullish coalescing is used for. now the 10 will be assign to the a
+console.log(a);
+
+// terinary operator -> condition ? true : false
+let score = 500
+score > 400 ? console.log("you are passed") : console.log("you are fail")
+
+
+
