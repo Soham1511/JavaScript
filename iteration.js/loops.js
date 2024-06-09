6a84b8d02148f24959029337cf9cf6bbeb74fa7b
Soham1511 <125867744+Soham1511@users.noreply.github.com>
2024-06-09 08:35:05 +0000

    while and do while loop

--- iteration.js/loops.js
+++ iteration.js/loops.js
@@ -32,10 +32,32 @@ for (let i = 1 ; i<=20 ; i++)
     {
         if(i == 15)
             {
-                console.log("15 detected");
+                //console.log("15 detected");
                 // break; it immediattly comes out of loop after conditon is achived
                 continue; // it continues to print the result after if statement
             }
-        console.log(i)
+        //console.log(i)
     }
 
+// ************************************* while loop ***********************************************//
+
+let i = 0;
+while(i <= 20)
+    {
+        console.log(`The even number between 1 to 20 is: ${i}`);
+        i = i+2;
+    }
+
+let myArr = ["iron man", "batman","hanuman","spiderman"];
+let j = 0;
+while(j < myArr.length)
+    {
+        console.log(`The superman is: ${myArr[j]}`);
+        j++;
+    }
+
+let score = 1;
+do{
+    console.log(`score is: ${score}`)
+    score++;
+}while(score <=10)
