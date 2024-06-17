da55b5c053174a34063d1f2aaeabe68184ab7422
Soham1511 <125867744+Soham1511@users.noreply.github.com>
2024-06-17 15:23:34 +0000

    assignment solution

--- project 2/script.js
+++ project 2/script.js
@@ -5,6 +5,7 @@ form.addEventListener('submit', (event)=>
     const height = parseInt(document.querySelector('#height').value);
     const weight = parseInt(document.querySelector('#weight').value); // the input value of height and weight will be parse into string
     const result = document.querySelector('#result'); 
+    const msg = document.querySelector('.msg');
 
     if(height === '' || height < 0 || isNaN(height))
         {
@@ -18,6 +19,18 @@ form.addEventListener('submit', (event)=>
            const bmi = (weight / ((height * height)/10000)).toFixed(2);
            // now display the result
            result.innerHTML = `<span>${bmi}</span>`
+           if(bmi < 18.6)
+               {
+                   msg.innerHTML = 'The BMI is less';
+               }
+           if(bmi > 18.6 && bmi < 24.9)
+               {
+                   msg.innerHTML = 'The BMI is in normal range';
+               }
+           if(bmi > 24.9)
+            {
+               msg.innerHTML = 'The BMI is more than normal range';
+            }
         }
-    
+
 })
